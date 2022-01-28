a9ac51b77bda596ffd5c6dee29d0475aaf052a1a
Aaron Zhang <bolun1@ualberta.ca>
2022-01-27 22:03:19 -0700

    upload1

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #  coding: utf-8 
 import socketserver
-
+import os
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -32,7 +32,48 @@ class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #self.request.sendall(bytearray("OK",'utf-8'))
+        decoded_data = self.data.decode('utf-8').split()
+        requested_path = decoded_data[1]
+        method = decoded_data[0]
+        #print(decoded_data)
+        if method != 'GET':
+            self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\n",'utf-8'))
+        else:
+            my_path = os.path.join(os.getcwd()+'/www' + requested_path)
+            if os.path.isfile(my_path):
+                if ".html" in requested_path:
+                    f = open(my_path)
+                    file = f.read()
+                    self.request.sendall(bytearray(f"HTTP/1.1 200 OK\r\ncontent-type: text/html\r\n\r\n{file}",'utf-8'))
+                    f.close()
+                elif ".css" in requested_path:
+                    f = open(my_path)
+                    file = f.read()
+                    self.request.sendall(bytearray(f"HTTP/1.1 200 OK\r\ncontent-type: text/css\r\n\r\n{file}",'utf-8'))
+                    f.close()
+                else:
+                    self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
+            elif os.path.isdir(my_path):
+                if my_path.endswith("/"):
+                    change_path = requested_path + "index.html"
+                    new_path = os.path.join(os.getcwd()+'/www' + change_path)
+                    f = open(new_path)
+                    file = f.read()
+                    self.request.sendall(bytearray(f"HTTP/1.1 200 OK\r\ncontent-type: text/html\r\n\r\n{file}",'utf-8'))
+                    f.close()
+                elif "/." or "/.." in requested_path:
+                    self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
+                else:
+                    change_path = requested_path + '/'
+                    self.request.sendall(
+                        bytearray("HTTP/1.1 301 Moved Permanently\r\nLocation: http://127.0.0.1:8080%s\r\n" % (change_path), 'utf-8'))
+            else:
+                self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n",'utf-8'))
+
+
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
